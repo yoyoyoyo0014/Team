7ab7a1b39beef44f8ed82baa5b5c5f8d0d5afb71
kcl0625 <pumpkinzzang123@gmail.com>
2024-10-02 17:39:51 +0900

    자동로그인 및 관련링크 추가

--- frontend_ebook/src/components/form/check.js
+++ frontend_ebook/src/components/form/check.js
@@ -1,6 +1,6 @@
-const Check = ({id, name, cls, label}) => {
+const Check = ({id, name, cls, label, style}) => {
 	return(
-		<div className="check">
+		<div className="check" style={style}>
 			<input type="checkbox" id={id} name={name} className={cls}/>
 			<label for={id}>{label}</label>
 		</div>
