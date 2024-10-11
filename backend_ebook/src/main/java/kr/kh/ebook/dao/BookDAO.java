40a956910283338079d85b261c35329054ab8634
yoyoyoyo0014 <yoyoyoyo0014@gmail.com>
2024-10-11 17:50:09 +0900

    책 검색기능 추가

--- backend_ebook/src/main/java/kr/kh/ebook/dao/BookDAO.java
+++ backend_ebook/src/main/java/kr/kh/ebook/dao/BookDAO.java
@@ -6,9 +6,11 @@ import org.apache.ibatis.annotations.Param;
 
 import kr.kh.ebook.model.vo.BookListVO;
 import kr.kh.ebook.model.vo.BookVO;
+import kr.kh.ebook.model.vo.GenreVO;
 import kr.kh.ebook.model.vo.ReviewVO;
 import kr.kh.ebook.model.vo.WriterVO;
 import kr.kh.ebook.pagination.BookCriteria;
+import kr.kh.ebook.pagination.BookPageMaker;
 import kr.kh.ebook.pagination.PageMaker;
 
 //https://github.com/st8324/java_240528/blob/main/servlet/servlet2/src/main/java/kr/kh/app/dao/PostMapper.xml
@@ -16,7 +18,7 @@ public interface BookDAO {
 
 	int count();  //테스트용 
 	
-	List<BookVO> searchBookList(PageMaker pm); //검색시 사용되는
+	List<BookVO> searchBookList(@Param("pm") BookPageMaker pm); //검색시 사용되는
 	
 	BookVO detailSelectBook(int num); //책 클릭 시 
 	
@@ -24,7 +26,7 @@ public interface BookDAO {
 
 	ReviewVO selectMyReview(@Param("userId")String userId,@Param("bookNum") int bookNum); //내 리뷰 찾기
 
-	int searchBookCount(BookCriteria bookCri);
+	int searchBookCount(@Param("country")String country,@Param("genre")int genre,@Param("search")String search);
 
 	int reviewCount(int bookNum);
 	
@@ -42,4 +44,6 @@ public interface BookDAO {
 	boolean updateReview(@Param("re")ReviewVO writeUserReview);  //리뷰 업데이트
 
 	boolean deleteReview(@Param("bookNum")int bookNum, @Param("id")String id);
+
+	List<GenreVO> selectGenreList();
 }
