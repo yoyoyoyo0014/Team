a769b2172988109a9157377c329cacdf2e64f567
yoyoyoyo0014 <yoyoyoyo0014@gmail.com>
2024-10-11 17:49:44 +0900

    Update MainController.java

--- backend_ebook/src/main/java/kr/kh/ebook/controller/MainController.java
+++ backend_ebook/src/main/java/kr/kh/ebook/controller/MainController.java
@@ -1,10 +1,17 @@
 package kr.kh.ebook.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.kh.ebook.model.vo.ReportTypeVO;
 import kr.kh.ebook.service.BookService;
+import kr.kh.ebook.service.ReportService;
 
 @Controller
 public class MainController {
@@ -17,4 +24,5 @@ public class MainController {
 		System.out.println(bookService.selectBook(2));
 		return "index";
 	}
+		
 }
