cb0e989c8069831e0758813e23adbcb0696ef9cf
saitrian <dhwodud200@gmail.com>
2024-10-11 16:41:38 +0900

    Update list.js

--- frontend_ebook/src/pages/post/list.js
+++ frontend_ebook/src/pages/post/list.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import moment from 'moment';
 
 const List = ({ communities = [] }) => {
   const { co_num } = useParams();
