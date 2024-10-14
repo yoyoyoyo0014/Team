6838e6eb8ee865f341e27a4f9a5691509f1c1473
saitrian <dhwodud200@gmail.com>
2024-10-14 16:50:59 +0900

    프록시 설정

--- frontend_ebook/src/setupProxy.js
+++ frontend_ebook/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/ebook', 
+    '/api', 
     createProxyMiddleware({
       target: 'http://localhost:8080/ebook',	//스프링 포트
       changeOrigin: true,
