b67b59018e29e09583ccd1663e15bb5ef98d482c
yoyoyoyo0014 <yoyoyoyo0014@gmail.com>
2024-10-22 17:28:59 +0900

    작가 기능

--- backend_ebook/src/main/java/kr/kh/ebook/model/vo/WriterVO.java
+++ backend_ebook/src/main/java/kr/kh/ebook/model/vo/WriterVO.java
@@ -8,7 +8,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class WriterVO {
-	public int wr_num;
+	public int wr_num = 0;
 	public String wr_name;
 	public String wr_profile;
+	
+	public WriterVO(String name, String profile) {
+		this.wr_name = name;
+		this.wr_profile = profile;
+	}
 }
