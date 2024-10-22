5d1f7d0d25acdb7a80c0df308ff292ea9d830fdc
yoyoyoyo0014 <yoyoyoyo0014@gmail.com>
2024-10-22 17:26:15 +0900

    도전과제

---
+++ backend_ebook/src/main/java/kr/kh/ebook/dao/AchievenentDAO.java
@@ -0,0 +1,21 @@
+package kr.kh.ebook.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import kr.kh.ebook.model.vo.BookGenreVO;
+import kr.kh.ebook.model.vo.BookListVO;
+import kr.kh.ebook.model.vo.BookSecondGenreVO;
+import kr.kh.ebook.model.vo.BookVO;
+import kr.kh.ebook.model.vo.BookGenreVO;
+import kr.kh.ebook.model.vo.ReviewVO;
+import kr.kh.ebook.model.vo.WriterListVO;
+import kr.kh.ebook.model.vo.WriterVO;
+import kr.kh.ebook.pagination.BookCriteria;
+import kr.kh.ebook.pagination.BookPageMaker;
+import kr.kh.ebook.pagination.PageMaker;
+
+//https://github.com/st8324/java_240528/blob/main/servlet/servlet2/src/main/java/kr/kh/app/dao/PostMapper.xml
+public interface AchievenentDAO {
+}
