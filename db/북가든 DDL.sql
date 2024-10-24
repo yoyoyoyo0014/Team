3d2bf6f0668fbedb915818ce865b6ce57162f966
yoyoyoyo0014 <yoyoyoyo0014@gmail.com>
2024-10-24 09:39:03 +0900

    bk_name , bk_isbn 길이 추가 rp_date default 값 추가 achievenent 컬럼 추가 `achievenent_List`  추가

--- db/북가든 DDL.sql
+++ db/북가든 DDL.sql
@@ -7,23 +7,25 @@ USE BookGarden;
 CREATE TABLE `member` (
 	`me_id`	varchar(255) primary key	NOT NULL,
 	`me_nickname`	varchar(8) unique	NOT NULL,
+    `me_name` varchar(10) NOT NULL,
 	`me_pw`	varchar(15)	NULL,
 	`me_email`	varchar(50) unique	NOT NULL,
-	`me_phone`	varchar(11)	NULL,
+	`me_phone`	varchar(20)	NULL,
 	`me_address`	text	NULL,
     `me_postalCode` varchar(5) NULL,
-	`me_gender` varchar(6) NOT NULL default 'male',
-	`me_birth`	date	NOT NULL,
+	`me_gender` varchar(6) NOT NULL default '선택안함',
+	`me_birth`	date	NULL,
 	`me_adult`	int	NOT NULL default '0',
 	`me_authority`	varchar(10)	NOT NULL default 'user',
 	`me_fail`	int	NULL,
 	`me_cookie`	varchar(255)	NULL,
 	`me_report`	int	NULL,
-	`me_ms_name`	varchar(10)	NOT NULL,
+	`me_ms_name`	varchar(10)	NOT NULL default '사용',
 	`me_stop`	datetime	NULL,
 	`me_cm`	varchar(20) unique	NULL,
 	`me_entercount`	int	NULL,
-	`me_last`	datetime	NULL
+	`me_last`	datetime	NULL,
+	`me_naverId`	varchar(255)	NULL
 );
 
 CREATE TABLE `review` (
@@ -31,21 +33,22 @@ CREATE TABLE `review` (
 	`re_content`	text	NOT NULL,
 	`re_bk_num`	int	NOT NULL,
 	`re_star`	double	NOT NULL,
-	`re_date`	datetime	NOT NULL  default CURRENT_TIMESTAMP,
+	`re_date`	datetime NOT NULL  default CURRENT_TIMESTAMP,
 	`re_me_id`	varchar(15)	NOT NULL
 );
 
 CREATE TABLE `book` (
 	`bk_num`	int primary key auto_increment	NOT NULL,
-	`bk_name`	varchar(50)	NOT NULL,
+	`bk_name`	varchar(255)	NOT NULL,
 	`bk_state`	varchar(4)	NOT NULL,
+	`bk_publisher`	varchar(50)	NOT NULL,
 	`bk_date`	datetime	NOT NULL,
 	`bk_sg_num`	int	NOT NULL,
 	`bk_plot`	longtext	NOT NULL,
 	`bk_price`	int	NOT NULL,
 	`bk_amount`	int	NOT NULL,
 	`bk_index`	varchar(255)	NULL,
-	`bk_isbn`	varchar(13)	NOT NULL,
+	`bk_isbn`	varchar(255)	NOT NULL,
 	`bk_score`	double	NULL,
 	`bk_reviewCount`	int	NULL,
 	`bk_totalPage`	int	NOT NULL,
@@ -74,7 +77,9 @@ CREATE TABLE `report` (
 	`rp_me_id`	varchar(15)	NOT NULL,
 	`rp_target`	varchar(50)	NOT NULL,
 	`rp_content`	text	NULL,
-	`rp_rt_num`	int	NOT NULL
+	`rp_rt_num`	int	NOT NULL,
+    `rp_date` datetime NOT NULL  default CURRENT_TIMESTAMP,
+    `rp_id` varchar(15) NOT NULL
 );
 
 CREATE TABLE `report_type` (
@@ -96,6 +101,7 @@ CREATE TABLE `cart` (
 
 CREATE TABLE `buy` (
 	`bu_num`	varchar(255) primary key	NOT NULL,
+	`bu_uid`	varchar(45)	NOT NULL,
 	`bu_me_id`	varchar(15)	NOT NULL,
 	`bu_state`	varchar(5)	NOT NULL,
 	`bu_payment`	varchar(15)	NOT NULL,
@@ -109,10 +115,11 @@ CREATE TABLE `post` (
 	`po_title`	varchar(50)	NOT NULL,
 	`po_content`	text	NOT NULL,
 	`po_me_id`	varchar(15)	NOT NULL,
+	`po_me_nickname`	varchar(8)	NOT NULL,
 	`po_date`	datetime	NOT NULL,
 	`po_co_num`	int	NOT NULL,
-	`po_view`	int	NULL,
-	`po_like`	int	NULL
+	`po_view`	int	NOT NULL default 0,
+	`po_like`	int	NOT NULL default 0
 );
 
 CREATE TABLE `community` (
@@ -123,8 +130,7 @@ CREATE TABLE `community` (
 CREATE TABLE `book_file` (
 	`bf_num`	int primary key auto_increment	NOT NULL,
 	`bf_name`	varchar(255)	NOT NULL,
-	`bf_bk_num`	int	NOT NULL,
-	`bf_type`	varchar(255)	NOT NULL
+	`bf_bk_num`	int	NOT NULL
 );
 
 CREATE TABLE `writer` (
@@ -136,13 +142,15 @@ CREATE TABLE `writer` (
 CREATE TABLE `achievenent` (
 	`ac_num`	int primary key auto_increment	NOT NULL,
 	`ac_title`	varchar(50)	NOT NULL,
-	`ac_info`	varchar(50)	NOT NULL
+	`ac_info`	varchar(50)	NOT NULL,
+    `ac_id` varchar(50) NOT NULL
 );
 
 CREATE TABLE `achievenent_List` (
 	`acl_ac_num`	int	NOT NULL,
 	`acl_me_id`	varchar(15)	NOT NULL,
-	`acl_date`	datetime	NOT NULL
+	`acl_date`	datetime	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `acl_check` varchar(2) NOT NULL DEFAULT 'X'
 );
 
 CREATE TABLE `book_List` (
@@ -164,7 +172,7 @@ CREATE TABLE `writer_Type` (
 );
 
 CREATE TABLE `buy_List` (
-	`bl_num`	varchar(255)	NOT NULL,
+	`bl_bu_num`	varchar(255)	NOT NULL,
 	`bl_bk_num`	int	NOT NULL,
 	`bl_me_id`	varchar(15)	NOT NULL
 );
@@ -323,7 +331,7 @@ REFERENCES `writer_Type` (
 );
 
 ALTER TABLE `buy_List` ADD CONSTRAINT `FK_buy_TO_buy_List_1` FOREIGN KEY (
-	`bl_num`
+	`bl_bu_num`
 )
 REFERENCES `buy` (
 	`bu_num`
@@ -363,4 +371,3 @@ ALTER TABLE `secondgenre` ADD CONSTRAINT `FK_genre_TO_secondgenre_1` FOREIGN KEY
 REFERENCES `genre` (
 	`ge_num`
 );
-
