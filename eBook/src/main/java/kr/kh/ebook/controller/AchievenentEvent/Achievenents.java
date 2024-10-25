64f26e270cd5b4699d0b978b733fd474b96565e7
yoyoyoyo0014 <yoyoyoyo0014@gmail.com>
2024-10-25 09:39:16 +0900

    도전과제 추가

---
+++ eBook/src/main/java/kr/kh/ebook/controller/AchievenentEvent/Achievenents.java
@@ -0,0 +1,10 @@
+package kr.kh.ebook.contoller.AchievenentEvent;
+
+
+import kr.kh.ebook.contoller.CheckAchievenent;
+import kr.kh.ebook.model.vo.AchievenentVO;
+
+public class Achievenents {
+	public AchievenentVO achievenentVo;
+	public CheckAchievenent CheckAchievenent;
+}
