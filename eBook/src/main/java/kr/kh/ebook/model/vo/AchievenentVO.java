bf1085ea24ca44bc4c44b8829dd63662bd669888
yoyoyoyo0014 <yoyoyoyo0014@gmail.com>
2024-10-25 09:27:51 +0900

    도전과제 모델 추가

---
+++ eBook/src/main/java/kr/kh/ebook/model/vo/AchievenentVO.java
@@ -0,0 +1,13 @@
+package kr.kh.ebook.model.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class AchievenentVO {
+	private int ac_num;
+	private String ac_title;
+	private String ac_info;
+	private String ac_id;
+}
