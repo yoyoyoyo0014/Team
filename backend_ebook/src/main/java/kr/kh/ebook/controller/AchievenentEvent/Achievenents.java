b45fb28ad402dc7d4512271d0aabc718d2e80668
yoyoyoyo0014 <yoyoyoyo0014@gmail.com>
2024-10-30 19:22:29 +0900

    도전과제 설명 (멋)

--- backend_ebook/src/main/java/kr/kh/ebook/controller/AchievenentEvent/Achievenents.java
+++ backend_ebook/src/main/java/kr/kh/ebook/controller/AchievenentEvent/Achievenents.java
@@ -4,6 +4,8 @@ package kr.kh.ebook.controller.AchievenentEvent;
 import kr.kh.ebook.model.vo.AchievenentVO;
 
 public class Achievenents {
+	// 도전과제 객체
 	public AchievenentVO achievenentVo;
+	// 달성 시 true를 반환하는 interface
 	public CheckAchievenent CheckAchievenent;
 }
