38fa5ee7f7da98856f3969dec3280e78cdb2e0ee
kcl0625 <pumpkinzzang123@gmail.com>
2024-11-04 11:24:32 +0900

    업적 샘플 데이터 수정

--- db/북가든_INSERT(샘플데이터).sql
+++ db/북가든_INSERT(샘플데이터).sql
@@ -20,4 +20,6 @@ values('공지제목','공지내용','admin123','2024-10-01','1');
 
 #도전과제 추가
 insert into achievenent(ac_title, ac_info,ac_id)
-values('첫 방문을 축하합니다!','처음 방문 시 달성','creatAccount');
+values('첫 방문을 축하합니다!','처음 방문 시 달성','creatAccount', 'fa-ribbon');
+insert into achievenent(ac_title, ac_info,ac_id)
+values('첫 구매를 축하합니다!','첫 구매 시 달성','buyBookCount=1', 'fa-credit-card');
