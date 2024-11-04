d0849fe360e04b9d526d2c223ca6040e5e75d218
kcl0625 <pumpkinzzang123@gmail.com>
2024-11-04 11:06:10 +0900

    도서 검색 구현중

--- frontend_ebook/src/components/book/bookSearch.js
+++ frontend_ebook/src/components/book/bookSearch.js
@@ -1,27 +1,25 @@
-import {useEffect, useState} from 'react';
-import MakePage from '../pageButton';
+import {useContext, useEffect, useState} from 'react';
+
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import MakePage from '../pageButton';
+import BookList from './booklist';
+import { GenreContext } from '../../context/GenreContext';
+import { Input } from '../form/input';
+import Button from '../form/button';
+
+let bannedSearchTerms =["#","%",";"]
+const helpSearch = "SearchWord="
 
 function BookSearch() {
+  const { bo_country } = useParams(0);
+  const { bo_genre } = useParams();
+  const { bo_category } = useParams();
+  const { bo_page } = useParams();
+  const { bo_search } = useParams();
+  const {genreList} = useContext(GenreContext);
 
-  //initCountry/:initGenre/:initSearch/:initCategory/:initPage
-
-  const {initCountry}= useParams();
-  const {initGenre}= useParams();
-  const {initSearch}= useParams();
-  const {initCategory}= useParams();
-  const {initPage}= useParams();
-
-  let [data, setData] = useState({
-    search : '',
-    country : 'all',
-    genre : 0,
-    category : '',
-    page : 0
-  })//검색 시 필요한 목록
-//Route path={"/searchBook/:initCountry/:initGenre/:initCategory/:initPage/SearchWord=:initSearch"}element ={<BookSearch/>}/>
   let [bookList,setBookList] = useState([])
-  //let [bookCount,setBookCount] = useState(0); //책 숫자
   
   let [search,setSearch] = useState('');
   let [country,setCountry] = useState("all");
@@ -36,10 +34,6 @@ function BookSearch() {
     next : false,
     pageList : []
   })//페이지 이동버튼
-  let[genreList,setGenreList] = useState([{
-  }])//장르 리스트
-
-  let bannedSearchTerms =["#","%",";"]
 
   function checkedCountry(e){
     setCountry(e.target.value);
@@ -53,200 +47,186 @@ function BookSearch() {
     setCategory(e.target.value);
   }//카테고리 설정
 
-  function getSearch(e){
-    setSearch(e.target.value);
-  }//검색 설정
-
-  function getGenreList(){
-    fetch('api/selectGenreList',{
-      method : "post",
-      //body : JSON.stringify(writeUserReview),
-      headers: {
-        'Content-Type': 'application/json',  // Content-Type 헤더 설정
-      },
-    })
-    .then(res=>res.json())
-    .then(genreListData=>{
-      // genreListData = 장르 리스트
-      if(genreListData){
-        setGenreList([...genreListData]);
-      }
-      
-    })
-    .catch(e=>console.error(e));
-  }//장르리스트 가져오는 함수
-
-  async function getSearchCount(){
-    for(var i = 0;i<bannedSearchTerms.length;i++){
-      if(search.indexOf(bannedSearchTerms[i]) !==-1){
-        alert('금지된 검색이 포함되어있습니다.')
-        return false;
-      }
-    }
-   
-
-    try {
-      // fetch 요청이 완료될 때까지 대기
-      const response = await fetch('api/searchBookCount/'+country+"/"+genre+"/SearchWord="+search,{
-        method : "post",
-        //body : JSON.stringify(writeUserReview),
-        headers: {
-          'Content-Type': 'application/json',  // Content-Type 헤더 설정
-        },
-      })
-      const searchCount = await response.text();
-      return searchCount;
-    } catch (e) {
-      console.error(e);
-      return false;;
-    }
-
-
-  }//검색개수 가져오기
-
-  async function selectSearch(){
-    for(var i = 0;i<bannedSearchTerms.length;i++){
-      if(search.indexOf(bannedSearchTerms[i]) !==-1){
-        alert('금지된 검색이 포함되어있습니다.')
-        return false;
-      }
-    }
-
-
-    try {
-      // fetch 요청이 완료될 때까지 대기
-      const response = await fetch('api/searchBook/'+category+"/"+country+'/'+
-        genre+'/'+page.currentPage+'/SearchWord='+search,{
-        method : "post",
-        //body : JSON.stringify(writeUserReview),
-        headers: {
-          'Content-Type': 'application/json',  // Content-Type 헤더 설정
-        },
-      })
-      const bookListData = await response.json();
-      return bookListData;
-    } catch (e) {
-      console.error(e);
-      return false;;
-    }
-
-  }//검색하기
-
   async function changePage(index){
     if(typeof index ==='number'){
       page.currentPage = index;
     }else{
-      
       var i ={
         index : 0
       }
       i = index;
       page.currentPage = i.index+1;
     }
+
     page = MakePage(page.contentsCount,page.currentPage);
-    await submitSearch();
-    
+
+    if(page.currentPage>page.endPage){
+      page.currentPage = page.endPage;
+      page = MakePage(page.contentsCount,page.currentPage);
+    }
+     
+   if(page.currentPage<=0)
+      page.currentPage = 1;
+
+   window.location.href =("/ebook/search/"+country+"/"+genre+"/"+category+"/"+page.currentPage+"/"+helpSearch+search);
   }//페이지 바꾸기
 
-  async function submitSearch(){
-    var searchCount = await getSearchCount();
-    if(!searchCount)
-      return;
+  async function submitSearch(country,genre,inputSerch = ''){
+    inputSerch =inputSerch.replace(helpSearch, "");
+
+    var searchCount = await getSearchCount(country,genre,inputSerch);
+
+    if(!searchCount) return;
+    
     page.contentsCount = searchCount;
     page = MakePage(page.contentsCount,page.currentPage);
-    var searchDataList = await selectSearch();
-    console.log(searchDataList)
+    var searchDataList = await selectSearch(country,genre,category,inputSerch,page);
     setBookList(searchDataList);
     setPage({...page});
   }//책 검색
 
+  function urlSetting(){
+    setCountry(bo_country)
+    setGenre(bo_genre)
+    setCategory(bo_category)
+
+    page.currentPage = bo_page;
+
+    if(page.currentPage<=0)
+      page.currentPage = 1;
+
+    setPage({...page})
+  }//url 세팅에 맞게 세팅
+  
+
   useEffect(()=>{
-
-    console.log(initCountry,initCategory,initGenre,initSearch,initPage)
-    setCountry(initCountry);
-    setGenre(initGenre);
-    setSearch(initSearch);
-
-    page.currentPage = initPage;
-    setPage({...page});
-    setCategory(initCategory);
-    
-    getGenreList();//장르 리스트 가져오기
-    submitSearch(); //그냥 검색
+    (async () => {
+      urlSetting();//url 세팅
+      submitSearch(bo_country,bo_genre,bo_search); //그냥 검색
+    })();
   },[]);
   //console.log('렌더링 횟수')
   return (
-    <div >
-      <input onChange={e=>getSearch(e)} placeholder="검색칸"></input>
-        <br/>
+    <div>
+      <Input type="text" change={setSearch} placeholder="검색칸"/>
+
+      <div>
         <div>
-          <label>
-              <input onClick={e=>checkedCountry(e)} type="radio" id="all" name="country" value="all" defaultChecked/>전체
-            </label>
-
-            <label>
-              <input onClick={e=>checkedCountry(e)} type="radio" id="domestic" name="country" value="domestic"/>국내
-            </label>
-
-            <label>
-              <input onClick={e=>checkedCountry(e)} type="radio" id="foreign" name="country" value="foreign"/>해외도서
-            </label>
-          <br/>
+          <input type="radio" name="country" id="all" value="all" defaultChecked={bo_country==='all'} onClick={e=>checkedCountry(e)} />
+          <label htmlFor="all">전체</label>
         </div>
         <div>
-        <label>
-           <input onClick={e=>getCategory(e)} type="radio" id="popularity" name="category" value="popularity" defaultChecked/>인기순
-          </label>
-          
-          <label>
-            <input onClick={e=>getCategory(e)}  type="radio" id="latest" name="category" value="latest"/>최신순
-          </label>
+          <input type="radio" name="country" id="domestic" value="domestic" defaultChecked={bo_country==='domestic'} onClick={e=>checkedCountry(e)} />
+          <label htmlFor="domestic">국내도서</label>
+        </div>
+        <div>
+          <input type="radio" name="country" id="foreign" value="foreign" defaultChecked={bo_country==='foreign'} onClick={e=>checkedCountry(e)} />
+          <label htmlFor="foreign">해외도서</label>
+        </div>
+        <div>
+          <input type="radio" name="category" id="popularity" value="popularity" defaultChecked={bo_category==='popularity'} onClick={e=>getCategory(e)} />
+          <label htmlFor="popularity">인기순</label>
+        </div>
+        <div>
+          <input type="radio" name="category" id="latest" value="latest" defaultChecked={bo_category==='latest'} onClick={e=>getCategory(e)} />
+          <label htmlFor="latest">최신순</label>
+        </div>
+        <div>
+          <input type="radio" name="category" id="orderPurchase" value="orderPurchase" defaultChecked={bo_category==='orderPurchase'} onClick={e=>getCategory(e)} />
+          <label htmlFor="orderPurchase">구매순</label>
+        </div>
+        <div>
+          <input type="radio" name="category" id="highPrice" value="highPrice" defaultChecked={bo_category==='highPrice'} onClick={e=>getCategory(e)} />
+          <label htmlFor="highPrice">높은 가격순</label>
+        </div>
+        <div>
+          <input type="radio" name="category" id="lowPrice" value="lowPrice" defaultChecked={bo_category==='lowPrice'} onClick={e=>getCategory(e)} />
+          <label htmlFor="lowPrice">낮은 가격순</label>
+        </div>
+        <div>
+          <input type="radio" name="category" id="rating" value="rating" defaultChecked={bo_category==='rating'} onClick={e=>getCategory(e)} />
+          <label htmlFor="rating">평점순</label>
+        </div>
+        <div>
+          <input type="radio" name="category" id="review" value="review" defaultChecked={bo_category==='review'} onClick={e=>getCategory(e)} />
+          <label htmlFor="review">리뷰순</label>
+        </div>
 
-          <label>
-          <input onClick={e=>getCategory(e)} type="radio" id="orderPurchase" name="category" value="orderPurchase"/> 구매 순
-          </label>
+      </div>
 
-          <label>
-            <input onClick={e=>getCategory(e)} type="radio" id="highPrice" name="category" value="highPrice"/>높은 가격 순
-          </label>
-
-         <label>
-          <input onClick={e=>getCategory(e)} type="radio" id="lowPrice" name="category" value="lowPrice"/>낮은 가격 순
-          </label>
-
-          <label>
-          <input onClick={e=>getCategory(e)} type="radio" id="rating" name="category" value="rating"/>평점 순
-          </label>
-
-          <label>
-          <input onClick={e=>getCategory(e)} type="radio" id="review" name="category" value="review"/>리뷰 순
+      {genreList.map((item,index)=>(
+          <label key={index} >
+            <input defaultChecked={bo_genre===index+1} onClick={()=>{checkedGenre(item.ge_num)}} type="radio" id={item.ge_num} name="genre" value={item.ge_num}/>{item.ge_name}
           </label> 
-        </div>
-          <br/>
-          {
-            genreList.map((item,index)=>(
-              <label key={index} >
-               <input onClick={()=>{checkedGenre(item.ge_num)}} type="radio" id={item.ge_num} name="genre" value={item.ge_num}/>{item.ge_name}
-             </label> 
-            ))
-          }
-        <input onClick={()=>changePage(1)} type="submit" value="제출"></input>
-        {
-          bookList.map((item,index)=>{
-            return (<div key={index}>{item.bk_name} : {item.bk_price}</div>)
-          })
-        }
-        
-        <button onClick={()=>changePage(page.currentPage-1)} disabled= {!page.prev}>이전</button>
+        ))
+      }
+      <input onClick={()=>{changePage(1)}} type="submit" value="제출"></input>
 
+      <BookList bookList={bookList}/>
+        
+      <Button click={()=>changePage(page.currentPage-1)} disabled= {!page.prev} text="이전"/>
       {page.pageList.map((item,index)=>{
           return(<button  onClick={()=>changePage({index})} disabled={page.currentPage==(index+1)} key={index}>{item}</button>)
       })}
 
-      <button onClick={()=>changePage(page.currentPage+1)} disabled = {!page.next}>다음</button>
-
+      <Button click={()=>changePage(page.currentPage+1)} disabled= {!page.next} text="다음"/>
     </div>
   );
 }
+async function getSearchCount(country,genre,inputSerch = ''){
+  inputSerch =inputSerch.replace("SearchWord=", "");
+  for(var i = 0;i<bannedSearchTerms.length;i++){
+    if(inputSerch.indexOf(bannedSearchTerms[i]) !==-1){
+      alert('금지된 검색이 포함되어있습니다.')
+      return false;
+    }
+  }
+  try {
+    // fetch 요청이 완료될 때까지 대기
+    const response = await fetch('/ebook/count/'+country+"/"+genre+"/"+'/SearchWord='+inputSerch,{
+      //body : JSON.stringify(writeUserReview),
+      headers: {
+        'Content-Type': 'application/json',  // Content-Type 헤더 설정
+      },
+    })
+    const searchCount = await response.text();
+    return searchCount;
+  } catch (e) {
+    console.error(e);
+    return false;;
+  }
+}//검색개수 가져오기
+
+async function selectSearch(country,genre,category,inputSerch = '',page){
+  inputSerch =inputSerch.replace("SearchWord=", "");
+
+
+  for(var i = 0;i<bannedSearchTerms.length;i++){
+    if(inputSerch.indexOf(bannedSearchTerms[i]) !==-1){
+      alert('금지된 검색이 포함되어있습니다.')
+      return false;
+    }
+  }
+  
+  try {
+    const response = await fetch('/ebook/search/'+category+"/"+country+'/'+
+      genre+'/'+page.currentPage+'/SearchWord='+inputSerch,{
+      //body : JSON.stringify(writeUserReview),
+      headers: {
+        'Content-Type': 'application/json',  // Content-Type 헤더 설정
+      },
+    })
+    const bookListData = await response.json();
+    console.log(bookListData)
+    if(bookListData ==null)
+      return false;
+    return bookListData;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+
+}//검색하기
+
 
 export default BookSearch;
