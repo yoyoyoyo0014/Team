8d18089b559487d8ae84308312193a7c39675e54
lhj10340 <lhj10340@naver.com>
2024-11-05 16:46:37 +0900

    < 프로젝트 진행 중 ... ( 11/05 )>
    브랜치 병합 중 ... ojy

--- frontend_ebook/src/components/book/bookList.js
+++ frontend_ebook/src/components/book/bookList.js
@@ -1,26 +1,119 @@
-import {useState} from 'react';
-import { Link, useLocation } from "react-router-dom";
-function BookList({readLastPage,LastPage}) {
-  let[useIsBuy, setUserIsBuy] = useState(false); //유저가 책을 샀는가
-  let [currentPage,setCurrentPage] = useState(0); //현재 페이지
-  let readLastPage;//마지막으로 읽은 페이지
-  let LastPage;//마지막 페이지
+import { Link } from 'react-router-dom';
+import '../../css/booklist.css';
+import axios from 'axios';
+import { useContext } from 'react';
+import { LoginContext } from '../../context/LoginContext';
+import Button from '../form/button';
 
-  function exit(){
-    if(readLastPage>currentPage){
-      //마지막 페이지를 현재 페이지로 정할것인가 묻기
+function BookList({bookList}) {
+  const { user } = useContext(LoginContext);
+
+  const addCart = (bk_num) => {
+    const options = {
+      url: '/cart/add',
+      method:'POST',
+      header: {
+        'Accept':'application/json',
+        'Content-Type': "'application/json';charset=UTF-8'"
+        //연결은 됐는데 보내는 타입이 맞지 않음(content type 점검)
+      },
+      data: {
+        ca_bk_num: bk_num,
+        ca_me_id: user?.me_id,
+      }
     }
+
+    axios(options)
+    .then(res => {
+      console.log(res);
+			alert('장바구니에 추가했습니다');
+		})
+		.catch((error) => {
+			console.log(error);
+		})
   }
 
-  function dataSubmit(savePage){
-    //데이터 베이스에 저장
-  }
   return (
-    <div >
-     
-
+    <div className="book-list">
+      <ul>
+     {Array.isArray(bookList) && bookList.length > 0 &&    bookList.map((item, index) => {
+         return (
+            <li className="theme-box" key={index}>
+              <div className="book-img"><Link to={"/ebook/selectBook/" + item.bk_num}><img src={'/img/book_'+ item.bk_num + '.jpg'} alt="불러오지 못한 이미지"  width="50" height="75" /></Link></div>
+              <div className="book-info">
+                <h3 className="title"><Link to={"/ebook/selectBook/" + item.bk_num}>{item.bk_name}</Link></h3>
+                <p className="writer">{item.bk_writer}</p>
+                <strong className="price">{Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(item.bk_price)}</strong>
+                <p>{item.bk_plot.lengt > 200 ? item.bk_plot.slice(0, 200) + '...' : item.bk_plot}</p>
+                <Button text="바로 구매" cls="btn btn-point"/>
+                <Button text="장바구니" click={() => {addCart(item.bk_num)}} cls="btn btn-dark"/>                
+              </div>
+            </li>
+            );
+          })
+      }
+      </ul>
     </div>
   );
+}
+
+//해당 책 페이지 번호 받아오기 없으면 -1
+export async function selectReadBook(bookNum,userId) {
+  var bookList ={
+    bl_me_id : userId,
+    bl_bk_num : bookNum,
+    bl_nowPage : 0
+  }
+  try{
+    const response = await fetch('/selectBook/currentBookPage',{
+      method: 'POST',  // POST 요청 설정
+      headers: {
+        'Content-Type': 'application/json',  // Content-Type 헤더 설정
+      },
+      body: JSON.stringify(bookList),
+    });
+    const res =await response.text();
+    return res;
+  }catch(e){
+    console.error(e);
+  }
+}
+
+//해당 책 페이지 업데이트
+export async function UpdateReadBook(userId,bookNum) {
+  var bookList ={
+    bl_me_id : userId,
+    bl_bk_num : bookNum,
+    bl_nowPage : 0
+  }
+  try{
+    const response = await fetch('/selectBook/updateBookPage',{
+      method: 'POST',  // POST 요청 설정
+      headers: {
+        'Content-Type': 'application/json',  // Content-Type 헤더 설정
+      },
+      body: JSON.stringify(bookList),
+    });
+    const res =await response.text();
+    return res;
+  }catch(e){
+    console.error(e);
+  }
+}
+
+//해당 페이지 가져오기
+export async function SelectBookshelf(userId,bookNum) {
+  try{
+    const response = await fetch('/selectBookList/selectBookshelfPage/'+userId+"/"+bookNum,{
+      headers: {
+        'Content-Type': 'application/json',  // Content-Type 헤더 설정
+      }
+    });
+    const res =await response.text();
+    return res;
+  }catch(e){
+    console.error(e);
+  }
 }
 
 export default BookList;
