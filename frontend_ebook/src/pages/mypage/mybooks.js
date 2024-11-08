438d93f0a918b00cbb863dd1b62e0564fbc1574c
lhj10340 <lhj10340@naver.com>
2024-11-08 11:05:29 +0900

    < 프로젝트 진행 중 ... ( 11/08 ) >
    팀원 요청에 따라 해당 css 및 js 는 팀원의 깃에서 복붙.

--- frontend_ebook/src/pages/mypage/mybooks.js
+++ frontend_ebook/src/pages/mypage/mybooks.js
@@ -1,10 +1,24 @@
-import { Fragment } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import MySubMenu from "../../components/mysubmenu";
 import '../../css/mypage.css';
 import '../../css/mybooks.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { LoginContext } from '../../context/LoginContext';
 
 const MyBooks = () => {
+	const { user } = useContext(LoginContext);
+	let [myBookList, setMyBookList] = useState([]);
+
+	useEffect(() => {
+		axios({
+			url: '/ebook/selectMyBook/' + user?.me_id,
+			method: 'get'
+		}).then(res => {
+			setMyBookList(res.data.myBookList);
+		})
+	}, [])
+
 	return (
 		<Fragment>
 			<MySubMenu/>
@@ -12,29 +26,23 @@ const MyBooks = () => {
 				<h2 className="section-title">내 책꽂이</h2>
 				<div className="bookshelf theme-box">	
 					<div className="container">
-						{[...Array(parseInt(3))].map((n, i) => {
-							return(<div className="row">
-								<div className="book">
-									<Link to={"/mypage/mybooks/book/" + (i + 1)}>
-										<div className="book-img"><img src="https://image.aladin.co.kr/product/34765/53/cover200/k632933028_1.jpg" alt="test" /></div>
-									</Link>
+						{/* 1234 5678 9101112 */}
+						{myBookList && [...Array(Math.ceil(myBookList.length/4))].map((n, i) => {
+							return(
+								<div className='row'>
+									{[...Array(parseInt(4))].map((book, j) => {
+										if (i > 0) j += 4;
+										if (j >= myBookList.length) return null;
+										return(
+											<div className="book">
+												<Link to={"/mypage/mybooks/book/" + myBookList[j].bk_num}>
+													<div className="book-img"><img src={'/img/book_'+ myBookList[j].bk_num + '.jpg'} alt={myBookList[j].bk_name}/></div>
+												</Link>
+											</div>
+										)
+								})}
 								</div>
-								<div className="book">
-									<Link to={"/mypage/mybooks/book/" + (i + 1)}>
-										<div className="book-img"><img src="https://image.aladin.co.kr/product/34765/53/cover200/k632933028_1.jpg" alt="test" /></div>
-									</Link>
-								</div>
-								<div className="book">
-									<Link to={"/mypage/mybooks/book/" + (i + 1)}>
-										<div className="book-img"><img src="https://image.aladin.co.kr/product/34765/53/cover200/k632933028_1.jpg" alt="test" /></div>
-									</Link>
-								</div>
-								<div className="book">
-									<Link to={"/mypage/mybooks/book/" + (i + 1)}>
-										<div className="book-img"><img src="https://image.aladin.co.kr/product/34765/53/cover200/k632933028_1.jpg" alt="test" /></div>
-									</Link>
-								</div>
-							</div>)
+							)
 						})}
 					</div>
 				</div>
